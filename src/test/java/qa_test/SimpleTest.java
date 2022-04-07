3ef044bac90301289985889a88a999aa1079e7e8
p.korolev <amlerok96@gmail.com>
2022-04-07 22:22:10 +0300

    init commit

---
+++ src/test/java/qa_test/SimpleTest.java
@@ -0,0 +1,13 @@
+package qa_test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void assertTrueTest() {
+        assertTrue(3>2);
+    }
+}
